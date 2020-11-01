2b2dbb3f920c41954425290e0b527e241e3d2a1d
Tatiana <lts221@yandex.ru>
2020-11-01 22:57:52 +0300

    gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,3 @@
-// "use strict";
-
-// var gulp = require("gulp");
-// var plumber = require("gulp-plumber");
-// var sourcemap = require("gulp-sourcemaps");
-// var sass = require("gulp-sass");
-// var postcss = require("gulp-postcss");
-// var autoprefixer = require("autoprefixer");
-// var server = require("browser-sync").create();
-
-// gulp.task("css", function () {
-//   return gulp.src("source/sass/style.scss")
-//     .pipe(plumber())
-//     .pipe(sourcemap.init())
-//     .pipe(sass())
-//     .pipe(postcss([
-//       autoprefixer()
-//     ]))
-//     .pipe(sourcemap.write("."))
-//     .pipe(gulp.dest("source/css"))
-//     .pipe(server.stream());
-// });
-
-// gulp.task("server", function () {
-//   server.init({
-//     server: "source/",
-//     notify: false,
-//     open: true,
-//     cors: true,
-//     ui: false
-//   });
-
-//   gulp.watch("source/sass/**/*.scss", gulp.series("css"));
-//   gulp.watch("source/*.html").on("change", server.reload);
-// });
-
-// gulp.task("start", gulp.series("css", "server"));
-
 "use strict";
 
 var gulp = require("gulp");
@@ -57,6 +19,9 @@ var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
 var jsmin = require("gulp-jsmin");
+
+var ghPages = require("gh-pages");
+var path = require("path");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -159,5 +124,10 @@ gulp.task("build", gulp.series(
   "jsmin",
   "html"
 ));
+
+function deploy(cb) {
+  ghPages.publish(path.join(process.cwd(), "./build"), cb);
+}
+exports.deploy = deploy;
 
 gulp.task("start", gulp.series("build", "server"));
